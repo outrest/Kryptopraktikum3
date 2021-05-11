648145e034cc5997ec49566d544dda6d1500b61f
outrest <floem@web.de>
2021-05-11 15:24:39 +0200

    Firsttry

--- src/LFSR.java
+++ src/LFSR.java
@@ -1,8 +1,15 @@
+import java.util.Arrays;
+import java.util.OptionalInt;
 
 public class LFSR {
     private boolean[] states;
 
     public LFSR(int[] polynom){
+        OptionalInt toppolynom = Arrays.stream(polynom).max();
+        if (toppolynom.isPresent()){
+            toppolynom=toppolynom.getAsInt();
+        }
+        states = new boolean[];
 
     }
 }
