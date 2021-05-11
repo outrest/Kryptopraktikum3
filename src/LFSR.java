4d32a2ea06546948ab439e3588ad3cb32534c878
outrest <floem@web.de>
2021-05-11 15:58:46 +0200

    Constructor

--- src/LFSR.java
+++ src/LFSR.java
@@ -3,11 +3,13 @@ import java.util.OptionalInt;
 
 public class LFSR {
     private boolean[] states;
+    private int[] polynom;
 
     public LFSR(int[] polynom){
         OptionalInt toppolynom = Arrays.stream(polynom).max();
+        int maxwert = 0;
         if (toppolynom.isPresent()){
-            toppolynom=toppolynom.getAsInt();
+            maxwert = toppolynom.getAsInt();
         }
         states = new boolean[];
 
