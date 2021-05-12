8d9aa5f8f51f9b7726a3c0516c0b26fb6a56e582
outrest <floem@web.de>
2021-05-12 16:50:20 +0200

    LFSR first master

--- src/Aufgabe1.java
+++ src/Aufgabe1.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class Aufgabe1 {
     public static void main(String[] args) {
@@ -5,6 +6,7 @@ public class Aufgabe1 {
         LFSR aufgabeA = new LFSR(polynom);
 
         aufgabeA.initialize(initialWert); //2097151 oder 111111111111111111111
+
         System.out.println("\nInitialisierung des LFSR mit den Werten: " + initialWert);
         String ausgabe = aufgabeA.shift(64);
         if (ausgabe.equals(vergleichsWert))
@@ -12,5 +14,22 @@ public class Aufgabe1 {
         else
             System.out.println("Das LFSR arbeitet nicht korrekt, die Werte unterscheiden sich: \n" + vergleichsWert +"\n" + ausgabe);
 
+
+        //b
+        LFSR aufgabeB = new LFSR(polynom);
+        aufgabeB.initialize(initialWert);
+        boolean[] saveStates = aufgabeB.getStates();
+        aufgabeA.initialize(initialWert);
+        int count = 0;
+        boolean[] tmp = new boolean[21];
+        aufgabeA.shift();
+        count++;
+        while(!(saveStates.equals(tmp))){
+            aufgabeA.shift();
+            tmp= aufgabeA.getStates();
+            count++;
+        }
+        System.out.println("Die maximale Periode ist bei " + count + " erreicht.");
+        System.out.println(aufgabeA);
     }
 }
