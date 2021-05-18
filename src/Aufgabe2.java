49577324d1a7a8a42e4315e68b6ae565f895493d
outrest <floem@web.de>
2021-05-18 16:24:37 +0200

    pushy

--- src/Aufgabe2.java
+++ src/Aufgabe2.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class Aufgabe2 {
     public static void main(String[] args) {
@@ -5,14 +6,32 @@ public class Aufgabe2 {
                 {0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8},
                 {4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0},
                 {15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13}};
-        DES sbox1 = new DES();
+        //DES sbox1 = new DES();
+        SBox sBox1 = new SBox(s1);
+        byte[] ergebnisse = new byte[64];
+        byte[][] testTabelle = new byte[64][16];
 
-        System.out.println(sbox1.innerBits((byte) 63));
-        System.out.println(sbox1.outerBits((byte) 63));
+        for (byte i = 0; i < 64; i++) {
+                byte trigger = sBox1.trigger(i);
+                ergebnisse[i] = trigger;
 
 
-       System.out.println( sbox1.getXORPartnerSum((byte)(12),(byte)(5)));
+                testTabelle[i][trigger]+=1;
 
+
+        }
+        System.out.println(Arrays.toString(ergebnisse));
+       // System.out.println(Arrays.deepToString(testTabelle));
+
+
+
+
+
+        byte[] gesamtVorkommen = new byte[16];
+        for (byte c : ergebnisse) {
+            gesamtVorkommen[c] += 1;
+        }
+        System.out.println(Arrays.toString(gesamtVorkommen));
 
     }
 }
